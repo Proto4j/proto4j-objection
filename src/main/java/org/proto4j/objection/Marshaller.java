3842bf413a3889a6617eff07f147d61b5e2876a1
MatrixEditor <leonard@lewedei.de>
2022-08-28 13:48:36 +0200

    Updated LICENSE pattern

--- src/main/java/org/proto4j/objection/Marshaller.java
+++ src/main/java/org/proto4j/objection/Marshaller.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2022 MatrixEditor
+ * Copyright (c) 2022 Proto4j
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
